5a7574845c1a2227aca7c24b2d1d68e4b5f70676
fneel <70952301+fneel@users.noreply.github.com>
2023-06-18 00:08:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//från dummyjson
+fetch("https://dummyjson.com/todos/add", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    todo: "Use DummyJSON in the project",
+    completed: false,
+    userId: 5,
+  }),
+})
+  .then((res) => res.json())
+  .then(console.log);
