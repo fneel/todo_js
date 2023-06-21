5467403665194c3c1ffd22ae158e297a13cd8e28
fneel <70952301+fneel@users.noreply.github.com>
2023-06-22 00:52:11 +0200

    skapa todo, lägga nya todos i lista, rendera tidigare sparade todos

--- main.js
+++ main.js
@@ -1,34 +1,73 @@
-const createTodo = document.getElementById("create-todo");
-const todoListItem = document.getElementById("todo-list");
-const doneListItem = document.getElementById("done-list");
-let todos = [];
+document.addEventListener("DOMContentLoaded", function () {
+  const createForm = document.getElementById("create-form");
+  const todoListItem = document.getElementById("todo-list");
+  const doneListItem = document.getElementById("done-list");
+  //let todoItems = [];
 
-function createTodo(description, todos, after) {
-  //från dummyjson
-  fetch("https://dummyjson.com/todos/add", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      todo: "Use DummyJSON in the project",
-      completed: false,
-      userId: 5,
-    }),
-  })
-    .then((res) => res.json())
-    .then((value) => {
-      value.id = idCounter++;
-      todos.push(value);
-      after();
-    });
-}
+  //hämta tidigare sparade todos 
+  var savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  savedTodos.forEach(function(todo) {
+    var todoItem = createTodoItem(todo.title, todo.descriptionm, todo.createdDate, todo.completedDate);
+    if (todo.completed) {
+      doneListItem.appendChild(todoItem);
+    } else {
+      todoListItem.appendChild(todoItem);
+    }
+    
+  });
 
-// createTodo.addEventListener("submit"), (event) => {
-//   event.preventDefault();
+// lyssnar efter submit, sen förhinra att sidan refreshar
+createForm.addEventListener("submit"), function (event) {
+   event.preventDefault();
 
+
+   var todoInput = createForm.querySelector('input');
+   var title = todoInput.value;
+   var description = todoInput.value;
+   var createdDate = new Date().toLocaleString();
+
+   if (title.trim() === '') {
+    alert('A title would be nice');
+    return;
+   }
+
+   //create a new todo
+   function createTodoItem(title, description, createdDate, completedDate) {
+      var todoItem = document.createElement('li');
+      todoItem.classList.add('todo-item');
+      todoItem.innerHTML = '<h3>' + title + '</h3><p>' + description + '</p><p>Created: ' + createdDate + '</p>';
+   }
+
+   //creates a <li> -element for new todos
+   var todoItem = createTodoItem(title, description, createdDate);
+   todoListItem.appendChild(todoItem);
+
+
+
+   };
+
+   // function createTodo(description, todos, after) {
+//   //från dummyjson
+//   fetch("https://dummyjson.com/todos/add", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       todo: "Use DummyJSON in the project",
+//       completed: false,
+//       userId: 5,
+//     }),
+//   })
+//     .then((res) => res.json())
+//     .then((value) => {
+//       value.id = idCounter++;
+//       todos.push(value);
+//       after();
+//     });
+// }
 //   let description = createTodo[0];
 // }
 
-//från 'freshman'
+// från 'freshman'
 // function addTodo(text) {
 //   const todo = {
 //     text,
@@ -42,15 +81,18 @@ function createTodo(description, todos, after) {
 //  //välj elementet 'form'
 // const form = document.querySelector('.js-form');
 // //submit todo
-// form.addEventListener('submit', event => {
+// createForm.addEventListener('submit', (event) => {
 // //förhindrar att sidan refreshar iom submit
 //   event.preventDefault();
-//   const text = input.value.trim();
-//   if (text !== '') {
-//     addTodo(text);
-//     input.value.trim();
-//     input.value = '';
-//     input.focus();
-//   }
+//   const input = document.querySelector('.js-todo-input');
 
-// });
+//tar värdet av input och trimmar det
+// const text = input.value.trim();
+// if (text !== '') {
+//   addTodo(text);
+//   input.value.trim();
+//   input.value = '';
+//   input.focus();
+// }
+
+//});
